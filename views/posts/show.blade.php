2325bcf49787e2722356722db5c4621c36ce2f00
ryanwinchester <fungku@gmail.com>
2016-03-14 16:32:19 -0700

    update links and add page

--- views/posts/show.blade.php
+++ views/posts/show.blade.php
@@ -1,5 +1,9 @@
 @extends('flashtag::layout')
 
+@section ('html-tag') <html itemscope itemtype="http://schema.org/Article"> @stop
+@section ('title') {{ $post->title }} @stop
+@section('meta') @include('flashtag::partials.post-meta') @stop
+
 @section('content')
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
@@ -12,13 +16,13 @@
                         <h2 class="subheading">{{ $post->subtitle }}</h2>
                         <span class="meta">
                             Published
-                            @if ($post->show_author && $post->author) by <a href="/authors/{{ $post->author->slug }}">{{ $post->author->name or 'Flashtag' }}</a> @endif
+                            @if ($post->show_author && $post->author) by <a href="{{ route('authors.show', [$post->author->slug]) }}">{{ $post->author->name or 'Flashtag' }}</a> @endif
                             on {{ $post->publishedOn() }}
-                            @if ($post->category) in <a href="/{{ $post->category->slug }}">{{ $post->category->name }}</a> @endif
+                            @if ($post->category) in <a href="{{ route('categories.show', [$post->category->slug]) }}">{{ $post->category->name }}</a> @endif
                         </span>
                         <span class="tags">
                             @foreach ($post->tags as $tag)
-                                <a href="/topics/{{ $tag->slug }}" class="label label-default">{{ $tag->name }}</a>
+                                <a href="{{ route('topics.show', [$tag->slug]) }}" class="label label-default">{{ $tag->name }}</a>
                             @endforeach
                         </span>
                     </div>
